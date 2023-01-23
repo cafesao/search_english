612360c3e552731a90da424f0b250dd9ecaff55b
cafesao <my@cafesao.net>
2023-01-23 19:10:32 -0300

    feat: add search

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
+function searchAll(word: string) {
+  window.open(
+    `https://translate.google.com.br/?hl=pt-BR&sl=en&tl=pt&text=${word}&op=translate`,
+    '_blank',
+    'noreferrer'
+  )
+  window.open(
+    `https://www.linguee.com.br/portugues-ingles/search?source=auto&query=${word}`,
+    '_blank',
+    'noreferrer'
+  )
+  window.open(
+    `https://www.google.com/search?q=${word}&tbm=isch`,
+    '_blank',
+    'noreferrer'
+  )
+  window.open(
+    `https://tatoeba.org/pt-br/sentences/search?from=eng&query=${word}&to=por`,
+    '_blank',
+    'noreferrer'
+  )
+  window.open(
+    `https://thesaurus.plus/thesaurus/${word}`,
+    '_blank',
+    'noreferrer'
+  )
+  window.open(`https://forvo.com/word/${word}/#en`, '_blank', 'noreferrer')
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [word, setWord] = useState('')
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="flex-col space-y-8 max-w-md">
+          <h1 className="text-5xl font-bold">Search English</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Word"
+              className="input input-bordered w-full max-w-xs"
+              onChange={(event) => setWord(event.target.value)}
+            />
+            <button
+              className="btn btn-primary mt-5"
+              onClick={() => {
+                searchAll(word)
+                setWord('')
+              }}
+            >
+              Search
+            </button>
+          </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
