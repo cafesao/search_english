190cfe7042ac761503aee00de4e2ce5cd16f68db
cafesao <my@cafesao.net>
2023-01-24 08:16:17 -0300

    feat: add select search

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,143 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-function searchAll(word: string) {
-  window.open(
-    `https://translate.google.com.br/?hl=pt-BR&sl=en&tl=pt&text=${word}&op=translate`,
-    '_blank',
-    'noreferrer'
-  )
-  window.open(
-    `https://www.linguee.com.br/portugues-ingles/search?source=auto&query=${word}`,
-    '_blank',
-    'noreferrer'
-  )
-  window.open(
-    `https://www.google.com/search?q=${word}&tbm=isch`,
-    '_blank',
-    'noreferrer'
-  )
-  window.open(
-    `https://tatoeba.org/pt-br/sentences/search?from=eng&query=${word}&to=por`,
-    '_blank',
-    'noreferrer'
-  )
-  window.open(
-    `https://thesaurus.plus/thesaurus/${word}`,
-    '_blank',
-    'noreferrer'
-  )
-  window.open(`https://forvo.com/word/${word}/#en`, '_blank', 'noreferrer')
+function searchAll(word: string, select: any) {
+  const objMap: any = {
+    googleTranslate: `https://translate.google.com.br/?hl=pt-BR&sl=en&tl=pt&text=${word}&op=translate`,
+    linguee: `https://www.linguee.com.br/portugues-ingles/search?source=auto&query=${word}`,
+    googleImage: `https://www.google.com/search?q=${word}&tbm=isch`,
+    tatoeba: `https://tatoeba.org/pt-br/sentences/search?from=eng&query=${word}&to=por`,
+    thesaurus: `https://thesaurus.plus/thesaurus/${word}`,
+    forvo: `https://forvo.com/word/${word}/#en`,
+  }
+  Object.keys(select)
+    .filter((value) => select[value] === true)
+    .map((value) => window.open(objMap[value], '_blank', 'noreferrer'))
 }
 
 function App() {
   const [word, setWord] = useState('')
+  const [select, setSelect] = useState({
+    googleTranslate: true,
+    linguee: true,
+    googleImage: true,
+    tatoeba: true,
+    thesaurus: true,
+    forvo: true,
+  })
 
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
-        <div className="flex-col space-y-8 max-w-md">
+        <div className="flex-col space-y-2 max-w-md">
           <h1 className="text-5xl font-bold">Search English</h1>
           <form>
             <input
               type="text"
               placeholder="Word"
-              className="input input-bordered w-full max-w-xs"
+              className="input input-bordered w-full max-w-xs mt-7"
               onChange={(event) => setWord(event.target.value)}
             />
+            <div className="form-control">
+              <div className="mt-10">
+                <label className="label cursor-pointer">
+                  <span className="label-text">Google Translate</span>
+                  <input
+                    type="checkbox"
+                    checked={select.googleTranslate}
+                    className="checkbox"
+                    onChange={() =>
+                      setSelect({
+                        ...select,
+                        googleTranslate: !select.googleTranslate,
+                      })
+                    }
+                  />
+                </label>
+                <label className="label cursor-pointer">
+                  <span className="label-text">Linguee</span>
+                  <input
+                    type="checkbox"
+                    checked={select.linguee}
+                    className="checkbox"
+                    onChange={() =>
+                      setSelect({
+                        ...select,
+                        linguee: !select.linguee,
+                      })
+                    }
+                  />
+                </label>
+                <label className="label cursor-pointer">
+                  <span className="label-text">Google Image</span>
+                  <input
+                    type="checkbox"
+                    checked={select.googleImage}
+                    className="checkbox"
+                    onChange={() =>
+                      setSelect({
+                        ...select,
+                        googleImage: !select.googleImage,
+                      })
+                    }
+                  />
+                </label>
+                <label className="label cursor-pointer">
+                  <span className="label-text">Tatoeba</span>
+                  <input
+                    type="checkbox"
+                    checked={select.tatoeba}
+                    className="checkbox"
+                    onChange={() =>
+                      setSelect({
+                        ...select,
+                        tatoeba: !select.tatoeba,
+                      })
+                    }
+                  />
+                </label>
+                <label className="label cursor-pointer">
+                  <span className="label-text">Thesaurus</span>
+                  <input
+                    type="checkbox"
+                    checked={select.thesaurus}
+                    className="checkbox"
+                    onChange={() =>
+                      setSelect({
+                        ...select,
+                        thesaurus: !select.thesaurus,
+                      })
+                    }
+                  />
+                </label>
+                <label className="label cursor-pointer">
+                  <span className="label-text">Forvo</span>
+                  <input
+                    type="checkbox"
+                    checked={select.forvo}
+                    className="checkbox"
+                    onChange={() =>
+                      setSelect({
+                        ...select,
+                        forvo: !select.forvo,
+                      })
+                    }
+                  />
+                </label>
+              </div>
+            </div>
+
             <button
               className="btn btn-primary mt-5"
               onClick={() => {
-                searchAll(word)
+                searchAll(word, select)
+                setSelect({
+                  googleTranslate: true,
+                  linguee: true,
+                  googleImage: true,
+                  tatoeba: true,
+                  thesaurus: true,
+                  forvo: true,
+                })
                 setWord('')
               }}
             >
